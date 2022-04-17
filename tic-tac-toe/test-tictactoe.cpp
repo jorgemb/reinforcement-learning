3003483b8f0baaee408bfaf91848e6d68acee8da
Jorge L. Martinez <jorelmb@gmail.com>
2022-04-17 22:02:23 +0200

    Working with Catch2 as submodule

--- tic-tac-toe/test-tictactoe.cpp
+++ tic-tac-toe/test-tictactoe.cpp
@@ -1,7 +1,5 @@
-#include <catch2/catch.hpp>
-#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
 
-int main( int argc, char* argv[] ) {
-    int result = Catch::Session().run( argc, argv );
-    return result;
+TEST_CASE("Dummy test case"){
+    REQUIRE(true);
 }
