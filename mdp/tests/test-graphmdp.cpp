e416dacb426f8cdcf050cd346e269355cb8a9925
Jorge L. Martinez <jorelmb@gmail.com>
2022-05-25 10:20:18 +0200

    Fixed graph-mdp tests

--- mdp/tests/test-graphmdp.cpp
+++ mdp/tests/test-graphmdp.cpp
@@ -23,7 +23,7 @@ std::ostream& operator<<(std::ostream& os, const Action& a){
 
 TEST_CASE("GraphMDP", "[graphmdp]") {
     GraphMDP<State, Action> g({Action::LEFT, Action::RIGHT});
-    std::vector<State> states{"Bad", "A", "B", "C", "D", "E", "Good"};
+    std::vector<State> states{"BAD", "A", "B", "C", "D", "E", "GOOD"};
 
     SECTION("Default values") {
         REQUIRE(g.get_states().size() == 0);
@@ -33,16 +33,39 @@ TEST_CASE("GraphMDP", "[graphmdp]") {
 
     SECTION("States"){
         // Insert a transition over every pair of states
-        std::vector<State> _ignore;
-        std::adjacent_difference(states.cbegin(), states.cend(), std::back_inserter(_ignore),
-                                 [&g](const auto& sA, const auto& sB){
-            g.add_transition(sA, Action::RIGHT, sB, 10.0, 1.0);
-            return sA;
-        });
+        auto iter_a = states.begin(), iter_b = std::next(iter_a), iter_end = states.end();
+        for(; iter_b != iter_end; ++iter_a, ++iter_b){
+            g.add_transition(*iter_a, Action::RIGHT, *iter_b, 10.0, 1.0);
+        }
 
+        // Default size
         REQUIRE(g.get_states().size() == states.size());
         auto state_match = Catch::Matchers::UnorderedEquals(states);
         REQUIRE_THAT(g.get_states(), state_match);
+
+        // Terminal states
+        State terminal{"BAD"};
+        REQUIRE_THROWS(g.set_terminal_state("ANY", 0.0));
+        REQUIRE_FALSE(g.is_terminal_state(terminal));
+        g.set_terminal_state(terminal, -10.0);
+        REQUIRE(g.is_terminal_state(terminal));
+        for(const auto& a: {Action::LEFT, Action::RIGHT}){
+            auto transitions = g.get_transitions(terminal, a);
+            REQUIRE(transitions.size() == 1);
+            auto [s, r, p] = transitions[0];
+            REQUIRE(s == terminal);
+            REQUIRE(r == -10.0_a);
+            REQUIRE(p == 1.0_a);
+        }
+
+        // Adding transitions to terminal states is illegal
+        REQUIRE_THROWS(g.add_transition(terminal, Action::LEFT, "A", 1.0, 1.0));
+
+        // Check terminal states list
+        std::vector<State> terminal_states{"BAD", "GOOD"};
+        for(auto s: terminal_states) g.set_terminal_state(s, 0.0);
+        auto terminal_match = Catch::Matchers::UnorderedEquals(terminal_states);
+        REQUIRE_THAT(g.get_terminal_states(), terminal_match);
     }
 
     SECTION("Transitions") {
@@ -84,6 +107,24 @@ TEST_CASE("GraphMDP", "[graphmdp]") {
             auto actions_matcher = Catch::Matchers::UnorderedEquals(available_actions);
             REQUIRE_THAT(actions, actions_matcher);
         }
+
+        SECTION("Transition probability"){
+            g.add_transition(A, Action::RIGHT, A, 10.0, 3.0);
+            g.add_transition(A, Action::RIGHT, B, 10.0, 1.0);
+            g.add_transition(A, Action::RIGHT, B, 10.0, 1.0);
+
+            REQUIRE(g.state_transition_probability(A, Action::RIGHT, B) == 0.40_a);
+        }
+    }
+
+    SECTION("Reward"){
+        State A{"A"}, B{"B"}, C{"C"};
+        g.add_transition(A, Action::LEFT, A, -10, 0.25);
+        g.add_transition(A, Action::LEFT, B, 50, 0.50);
+        g.add_transition(A, Action::LEFT, C, 5, 0.25);
+
+        double expected_reward = -10.0*0.25 + 50*0.5 + 5*0.25;
+        REQUIRE(g.expected_reward(A, Action::LEFT) == Approx(expected_reward));
     }
 
 }
